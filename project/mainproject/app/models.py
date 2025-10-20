da886b55f1a7671ce8178e5e60bc0128b1041322
alto <uzukialto@gmail.com>
2025-10-20 17:18:05 +0900

    [update] model整形

--- project/mainproject/app/models.py
+++ project/mainproject/app/models.py
@@ -1,31 +1,18 @@
 from django.db import models
 from django.conf import settings
 
-
-class Class_cancellation(models.Model):
-    date = models.DateTimeField("休講情報")
-    
-class Homework(models.Model):
-    deadline = models.DateTimeField("締め切り")
-    content = models.TextField("内容", blank=True)
-    
-class Memo(models.Model):
-    content = models.TextField("内容", blank=True)
-    # image = models.ImageField("画像", upload_to='memo_images/', blank=True, null=True)
-    
 class Class(models.Model):
     class_name = models.CharField("授業名", max_length=100)
     professor_name = models.CharField("教授名", max_length=100)
     classroom_name = models.CharField("教室名", max_length=100)
     
     day_of_the_week = models.IntegerField("曜日", choices=[
-        (0, '日曜日'),
-        (1, '月曜日'),
-        (2, '火曜日'),
-        (3, '水曜日'),
-        (4, '木曜日'),
-        (5, '金曜日'),
-        (6, '土曜日'),
+        (0, '月曜日'),
+        (1, '火曜日'),
+        (2, '水曜日'),
+        (3, '木曜日'),
+        (4, '金曜日'),
+        (5, '土曜日')
     ])
     period = models.IntegerField("時限", choices=[
         (1, "1限目"),
@@ -42,10 +29,7 @@ class Class(models.Model):
     absence_count = models.IntegerField("欠席回数", default=0)
     total_late_and_early_leave_count = models.IntegerField("遅刻・早退回数", default=0)
     belongings = models.TextField("持ち物", blank=True)
-    class_cancellations = models.ForeignKey(Class_cancellation, on_delete=models.CASCADE, verbose_name="休講情報", blank=True, null=True)
-    homeworks = models.ForeignKey(Homework, on_delete=models.CASCADE, verbose_name="宿題", blank=True, null=True)
     examination = models.TextField("試験日程", blank=True)
-    memos = models.ForeignKey(Memo, on_delete=models.CASCADE, verbose_name="メモ", blank=True, null=True)
     
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -58,3 +42,30 @@ class Class(models.Model):
         return self.class_name
     
 
+
+class Class_cancellation(models.Model):
+    date = models.DateTimeField("休講情報")
+    class_model = models.ForeignKey(Class, on_delete=models.CASCADE, blank=True, null=True)
+    
+    def __str__(self):
+        return f"休講日: {self.date.strftime('%Y-%m-%d')}"
+
+    
+class Homework(models.Model):
+    deadline = models.DateTimeField("締め切り")
+    content = models.TextField("内容", blank=True)
+    classModel = models.ForeignKey(Class, on_delete=models.CASCADE, blank=True, null=True)
+    
+    def __str__(self):
+        return self.content[:20]  # 先頭の20文字を表示
+
+    
+class Memo(models.Model):
+    content = models.TextField("内容", blank=True)
+    # image = models.ImageField("画像", upload_to='memo_images/', blank=True, null=True)
+    class_model = models.ForeignKey(Class, on_delete=models.CASCADE,  blank=True, null=True)
+    
+    def __str__(self):
+        return self.content[:20]  # 先頭の20文字を表示
+
+    
--- project/mainproject/app/views.py
+++ project/mainproject/app/views.py
@@ -4,17 +4,37 @@ from django.http import HttpResponse
 from .forms import ClassForm
 from django.contrib.auth.decorators import login_required
 
-def index(request):
+def get_class_context():
+    context = {}
     classes = Class.objects.all()
-    context = {
-        "classes": classes,
-    }
+    class_list = []
+    exist_saturday_class = False
+    class_count = 6 * 8  # 6日間、各8時限
+    subject = classes[0]
+    
+    for i in range(class_count):
+        if(subject.day_of_the_week == i % 6 and subject.period == (i % 8) + 1):
+            class_list.append(subject)
+            if subject.day_of_the_week == 5:  # 土曜日のクラスが存在するか確認
+                exist_saturday_class = True
+            try:
+                subject = classes[classes.index(subject) + 1]
+            except IndexError:
+                subject = None
+        else:
+            class_list.append(None)
+            
+    context["class_list"] = class_list
+    context["exist_saturday_class"] = exist_saturday_class
+    return context
+
+def index(request):
+    context = get_class_context()
     return render(request, "app/index.html", context=context)
 
 @login_required
 def home_edit(request):
-    classes = Class.objects.all()
-    context = {"classes": classes}
+    context = get_class_context()
     
     if request.method == "POST":
         form = ClassForm(request.POST)
